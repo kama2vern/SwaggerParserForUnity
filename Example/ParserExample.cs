ad191fa2daa75cfbd70d51d39f5a21928cc961dc
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-11-15 18:59:25 +0900

    Add parse examples

---
+++ Example/ParserExample.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using SwaggerParser;
+
+public class ParserExample : MonoBehaviour {
+	void Start () {
+		Swagger swagger = SwaggerParser.Swagger.parse("Assets/Example/swagger.yaml");
+		
+		Debug.Log(swagger.basePath); // => "/v2"
+		Debug.Log(swagger.host); // => "petstore.swagger.io"
+	}
+}
